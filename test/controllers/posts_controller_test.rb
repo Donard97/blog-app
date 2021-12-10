620e35b881d2e593d9fa0b7bfcb76841bfcf386a
Donard97 <donardgola@gmail.com>
2021-12-10 13:58:20 +0100

    Fix rubocop errors

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,13 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get :index" do
-    get posts_:index_url
+  test 'should get :index' do
+    get posts_: index_url
     assert_response :success
   end
 
-  test "should get :show" do
-    get posts_:show_url
+  test 'should get :show' do
+    get posts_: show_url
     assert_response :success
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get :index" do
-    get users_:index_url
+  test 'should get :index' do
+    get users_: index_url
     assert_response :success
   end
 
-  test "should get :show" do
-    get users_:show_url
+  test 'should get :show' do
+    get users_: show_url
     assert_response :success
   end
 end
