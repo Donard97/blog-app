839c27158bf5022dfccd77abc1b7a73145711c11
Donard97 <donardgola@gmail.com>
2021-12-13 16:18:30 +0100

    Set up associations for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', counter_cashe :posts_count
+  has_many :comments, dependent :destroy
+  has_many :likes, dependent :detroy
 end
