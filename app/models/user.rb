295cb2156ce5443288bbdb943abfa71803ac5c74
Donard97 <donardgola@gmail.com>
2021-12-13 16:15:14 +0100

    Set up associations for users and a method to return 3 posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has.many :posts, foreign_key: 'author_id'
+  has.many :comments, foreign_key: 'author_id'
+  has.many :likes, foreign_key: 'author_id'
+
+  def recent_posts
+    posts.last(3)
+  end
 end
