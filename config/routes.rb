c89b9c39d5b946e5129a17f59169f4ef08313110
Donard97 <donardgola@gmail.com>
2021-12-13 13:59:12 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get '/users', to: 'users#index'
-get '/users/:id', to: 'users#show'
-get '/users/:id/posts', to: 'posts#index'
-get '/users/:id/posts/:id', to: 'posts#show'
+  resources :users, only: [:index, :show] do
+    esources :posts, only: [:index, :show]
+  end
 end
