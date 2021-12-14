014f6185d3cb9c3803ab80f247153f98886ed280
Donard97 <donardgola@gmail.com>
2021-12-14 11:29:54 +0100

    Update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :author, foreign_key: 'User'
+  belongs_to :author, class_name: 'User'
   belongs_to :post, foreign_key: 'post_id'
 
   def update_comments_counter
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :author, foreign_key: 'User'
+  belongs_to :author, class_name: 'User'
   belongs_to :post, foreign_key: 'post_id'
 
   def update_likes_counter
