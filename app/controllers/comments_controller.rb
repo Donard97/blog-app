0975c9cd83eda138a6c9b7fd9218f3237f5e8049
Donard97 <donardgola@gmail.com>
2021-12-18 00:23:59 +0100

    Fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,17 @@
 class CommentsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
-    def create
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.create(text: params[:text], author_id: current_user.id, post_id: @post.id)
-  
-      respond_to do |format|
-        format.html do
-          if @comment.save
-            redirect_to user_post_path(@post.author.id, @post.id), notice: 'Comment created successfully'
-          else
-            redirect_to user_post_path(@post.author.id, @post.id), alert: 'Comment not created, try again!'
-          end
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(text: params[:text], author_id: current_user.id, post_id: @post.id)
+
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          redirect_to user_post_path(@post.author.id, @post.id), notice: 'Comment created successfully'
+        else
+          redirect_to user_post_path(@post.author.id, @post.id), alert: 'Comment not created, try again!'
         end
       end
     end
   end
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,5 +25,4 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
-
 end
