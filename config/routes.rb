ed9f9231e5fb123f8a942ec87e85ebf0eb00cd0d
Donard97 <donardgola@gmail.com>
2021-12-17 22:00:33 +0100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]
   end
 end
