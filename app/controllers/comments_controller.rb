a700a12589dce7509b5350221bba748eed8a7f7a
Donard97 <donardgola@gmail.com>
2021-12-17 23:45:48 +0100

    Create comments controllers and add create method for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,20 @@
 class CommentsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
-    @comments = @user.comments
-  end
-
-  def show
-    @comment = Comment.find(params[:id])
-  end
-
-  def new
-    @comment = Comment.new
-  end
-
+  skip_before_action :verify_authenticity_token
   def create
-    @comment = Comment.new(params[:id])
-    @comment.text = params[:text]
-    @comment.author_id = current_user.id
-    if @comment.save
-      flash[:notice] = 'Comment successfully added!'
-      redirect_to user_posts_path
-    else
-      render 'new'
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.create(text: params[:text], author_id: current_user.id, post_id: @post.id)
+  
+      respond_to do |format|
+        format.html do
+          if @comment.save
+            redirect_to user_post_path(@post.author.id, @post.id), notice: 'Comment created successfully'
+          else
+            redirect_to user_post_path(@post.author.id, @post.id), alert: 'Comment not created, try again!'
+          end
+        end
+      end
     end
   end
+
 end
