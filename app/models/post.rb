d47cfb353cd4ce2c102923639aa200038e54d8bd
Donard97 <donardgola@gmail.com>
2021-12-18 12:55:56 +0100

    Fix user validations and add validations for posts, likes and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_ineger: true, geater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_ineger: true, geater_than_or_equal_to: 0 }
+
+
   def update_post_counter
     user.increment!(:posts_counter)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  validate :name, presence:true
-  validate :posts_counter, numericality : { only_integer: true, greater_than_or_equal_to: 0}
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
