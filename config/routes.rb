c760ff92723eddfe3ce0084935d225ac8e2dfc2c
Donard97 <donardgola@gmail.com>
2021-12-22 13:55:16 +0100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :show, :create] do
-      resources :comments, only: [:new, :create]
+  end
+
+  resources :posts do
+    resources :comments, only: [:new, :create]
     end
   end
 end
