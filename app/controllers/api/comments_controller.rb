821586117afd1a1124986271e3722c07d1055215
Donard97 <donardgola@gmail.com>
2021-12-25 21:30:52 +0100

    Fix funcionality to post comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -7,18 +7,18 @@ class Api::CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_parameters.merge(user_id: current_user.id))
+    @comment = @post.comments.new(text: comment_params[:text], user: current_user)
+
     if @comment.save
-      render json: { success: true, message: 'Comment created', data: { comment: @comment } }
+      render json: { success: true, data: { comment: @comment } }, status: :created
     else
-      render json: {success: false, message: 'Not created'}      
+      render json: { success: false, errors: @comment.errors }, status: :bad_request
     end
   end
 
-
   private
 
-  def comment_parameters
+  def comment_params
     params.require(:comment).permit(:text)
   end
 end
